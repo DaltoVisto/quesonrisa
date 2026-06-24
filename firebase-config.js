7acb8deab33ce57956fff89ce5f0759b66c32123
josefina <salazarjosefina53@gmail.com>
2026-06-24 16:04:58 +0200

    fin

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2lhun7fPQmAdsY8FoSegi89D8jy83Oeg",
+  authDomain: "quesonrisa-2de8f.firebaseapp.com",
+  projectId: "quesonrisa-2de8f",
+  storageBucket: "quesonrisa-2de8f.firebasestorage.app",
+  messagingSenderId: "944850520581",
+  appId: "1:944850520581:web:7b0f8da836f83b8b2bea6c",
+  measurementId: "G-CD6VP2L9E8",
+};
